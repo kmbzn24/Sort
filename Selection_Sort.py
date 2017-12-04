1dd9437a9184dbd7c342ebdbf848dc1f52c3dea4
Labs180 <qudwns0524@gmail.com>
2017-12-04 23:56:17 +0900

    Update Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,16 +1,19 @@
-list = [];
-n = int(input());
-for i in range(0, n) :
-    list += [int(input())];
 def selection_sort(array) :
     for i in range(0, len(array)) :
         min = float("inf");
-        for j in range(0, len(array) - i) :
+        for j in range(i, len(array)) :
             if (array[j] < min) :
                 min = array[j];
                 loc = j;
-        del array[loc];
-        array = array + [min];
+        sav = array[i];
+        array[i] = min;
+        array[loc] = sav;
     for i in range(0, len(array)) :
         print(array[i]);
-selection_sort(list);
+
+list = []; #리스트 생성
+n = int(input()); #1번째 줄에서 리스트의 길이값을 입력받는다.
+for i in range(0, n) :
+    list += [int(input())]; #2번째 줄부터 n + 1번째 줄까지 값을 받고 리스트로 만든다.
+
+selection_sort(list); #정의된 함수 실행
