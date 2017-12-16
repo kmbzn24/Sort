52812503ebce818afe5cc59959befe41f47c580c
김병준 <qudwns0524@gmail.com>
2017-12-16 13:56:57 +0900

    Update Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -24,3 +24,10 @@ def merge(front, rear) :
             result.append(front[i]);
             i += 1;
     return result;
+
+list = []; #리스트 생성
+n = int(input()); #1번째 줄에서 리스트의 길이값을 입력받는다.
+for i in range(0, n) :
+    list += [int(input())]; #2번째 줄부터 n + 1번째 줄까지 값을 받고 리스트로 만든다.
+
+merge_sort(list); #정의된 함수 실행
